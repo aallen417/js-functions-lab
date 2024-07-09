8ea706aacca3bd66e1368b2fde4fc95f1ad01678
aallen417 <austin.allen417@gmail.com>
2024-07-09 18:41:07 -0400

    Accidentally completed js functions lab exercise 4 as exercise 3 now fixed both complete

--- script.js
+++ script.js
@@ -54,8 +54,31 @@ Example: isCharAVowel('a') should return true.
 Complete the exercise in the space below:
 */
 
+const isCharAVowel = (character) => {
+    if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u") {
+        return true
+    }   else {
+        return false
+    }
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
 const generateEmail = (name, domain) => {
     return name + "@" + domain
 }
 
-console.log("Exercise 3 Result:", generateEmail("johnsmith", "example.com"))
+console.log("Exercise 4 Result:", generateEmail("johnsmith", "example.com"))
